e96beaf208529e230569b3d9be16c776eff22472
repoficialx <raytrucos@icloud.com>
2026-06-23 22:28:17 +0200

    new recipe format

--- RecipCore/read.cs
+++ RecipCore/read.cs
@@ -30,4 +30,46 @@ public class RecipApp
 
         return sectionLines;
     }
+    public static void ReadFile_v2(string filePath, out string recipeName, out List<Ingrediente> ingredientes, out List<Paso> pasos, out Metadata metadata, out int ver)
+    {
+        var receta = Newtonsoft.Json.JsonConvert.DeserializeObject<RecetaV2>(File.ReadAllText(filePath));
+
+        recipeName = receta.name;
+        ingredientes = receta.ingredients;
+        pasos = receta.steps;
+        metadata = receta.metadata;
+        ver = receta.version;
+    }
 }
+
+#pragma warning disable
+public class RecetaV2
+{
+    public int version { get; set; } = 2;
+    public string name { get; set; }
+    public List<Ingrediente> ingredients { get; set; }
+    public List<Paso> steps { get; set; }
+    public Metadata metadata { get; set; }
+}
+
+public class Ingrediente
+{
+    public string name { get; set; }
+    public double quantity { get; set; }
+    public string unit { get; set; }
+}
+
+public class Paso
+{
+    public string text { get; set; }
+    public int time { get; set; } // en segundos
+    public string image { get; set; }
+}
+
+public class Metadata
+{
+    public string id { get; set; }
+    public string source { get; set; }
+    public string createdAt { get; set; }
+}
+#pragma warning restore
